3a03b266b84acb8f9580962ece522e1ca46b2a8f
maithaalhayyas <maithaalhayyas@gmail.com>
2021-06-30 23:15:21 +0400

    get the numbers from user and print the sum of them

--- prod.c
+++ prod.c
@@ -8,6 +8,15 @@ int my_greeter12() {
 
     int a,b,sum;
 
+    printf("Enter 1st Number: ");
+    scanf("%d", &a);
+
+    printf("Enter 2st Number:  ");
+    scanf("%d", &b);
+
+    sum =  a +  b;
+    printf("Hello World! Your sum is %d\n",sum);
+
 
 
 }
